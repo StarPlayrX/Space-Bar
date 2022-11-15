6637ac906284c9fede074a0960acaed93dee419d
Todd Bruss <admin@starplayrx.com>
2022-11-15 09:45:25 -0500

    Thrust and Air Breaks
    No more ludicrous speed

--- Space-Bar/Game Scene/Physics/UpdateScene.swift
+++ Space-Bar/Game Scene/Physics/UpdateScene.swift
@@ -30,14 +30,28 @@ extension GameScene {
                 return
             }
             
-            //MARK: Clamp helps prevent up and down ball movement from taking place for less than 5 seconds
-            if body.velocity.dy > -99 && body.velocity.dy < 99                                         {
+            let level = CGFloat(settings.currentlevel + 1)
+            let half  = CGFloat(settings.currentlevel + 1) / 2
+            let quart = CGFloat(settings.currentlevel + 1) / 4
+
+            //MARK: Min Speed dy (Thrust)
+            if abs(body.velocity.dy) < CGFloat(99 + half) {
                 body.velocity.dy <= zero ? body.applyImpulse(CGVector(dx: 0, dy: -25)) : body.applyImpulse(CGVector(dx: 0, dy: 25))
             }
             
-            //MARK: Clamp helps prevent side to side ball movement from taking place for less than 5 seconds
-            if body.velocity.dx > -49 && body.velocity.dx < 49                                        {
+            //MARK: Min Speed dx (Thrust)
+            if abs(body.velocity.dx) < CGFloat(99 + quart) {
                 body.velocity.dx <= zero ? body.applyImpulse(CGVector(dx: -12.5, dy: 0)) : body.applyImpulse(CGVector(dx: 12.5, dy: 0))
+            }
+            
+            //MARK: Max Speed dy (Breaks)
+            if abs(body.velocity.dy) > CGFloat(1199 + level) {
+                body.velocity.dy <= zero ? body.applyImpulse(CGVector(dx: 0, dy: 12.5)) : body.applyImpulse(CGVector(dx: 0, dy: -12.5))
+            }
+            
+            //MARK: Max Speed dx (Breaks)
+            if abs(body.velocity.dx) > CGFloat(1199 + half) {
+                body.velocity.dx <= zero ? body.applyImpulse(CGVector(dx: 25, dy: 0)) : body.applyImpulse(CGVector(dx: -25, dy: 0))
             }
         }
     }
